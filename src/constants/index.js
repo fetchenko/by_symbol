d0a1483b6a43206edf42fb5ba511a21448ab314f
fetchenko <30293847+fedchenko273@users.noreply.github.com>
2020-01-29 20:08:15 +0300

    changed image paths

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,25 +3,25 @@ export const SYMBOL_OPTIONS = [
     label: "Symbol of spring and youth",
     path: "symbol-of-spring",
     title: "spring and youth",
-    img: "/assets/img/symbol-of-spring.png"
+    img: "./assets/img/symbol-of-spring.png"
   },
   {
     label: "Symbol of wealth",
     path: "symbol-of-wealth",
     title: "wealth",
-    img: "/assets/img/symbol-of-wealth.png"
+    img: "./assets/img/symbol-of-wealth.png"
   },
   {
     label: "Union of the sun and the earth. Symbol of harvest",
     path: "symbol-of-harvest",
     title: "harvest",
-    img: "/assets/img/symbol-of-harvest.png"
+    img: "./assets/img/symbol-of-harvest.png"
   },
   {
     label: "Symbol of ancestors",
     path: "symbol-of-ancestors",
     title: "ancestors",
-    img: "/assets/img/symbol-of-ancestors.png"
+    img: "./assets/img/symbol-of-ancestors.png"
   }
 ];
 
@@ -30,6 +30,6 @@ export const COMPOSITIONS = [
     label: "Symbol of love",
     path: "symbol-of-love",
     title: "symbol of love",
-    img: "/assets/img/symbol-of-love.png"
+    img: "./assets/img/symbol-of-love.png"
   }
 ];
