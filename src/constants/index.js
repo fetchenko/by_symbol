17cec13bc82a4f9619adeb97b3bfc34aec4a252b
fetchenko <30293847+fedchenko273@users.noreply.github.com>
2020-01-29 20:25:14 +0300

    changed images paths

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,27 +1,32 @@
+const imgDir =
+  process.env.NODE_ENV === "development"
+    ? "/assets/img/"
+    : "/belorussian-symbols/assets/img/";
+
 export const SYMBOL_OPTIONS = [
   {
     label: "Symbol of spring and youth",
     path: "symbol-of-spring",
     title: "spring and youth",
-    img: "./belorussian-symbols/assets/img/symbol-of-spring.png"
+    img: `${imgDir}/symbol-of-spring.png`
   },
   {
     label: "Symbol of wealth",
     path: "symbol-of-wealth",
     title: "wealth",
-    img: "/belorussian-symbols/assets/img/symbol-of-wealth.png"
+    img: `${imgDir}/symbol-of-wealth.png`
   },
   {
     label: "Union of the sun and the earth. Symbol of harvest",
     path: "symbol-of-harvest",
     title: "harvest",
-    img: "./assets/img/symbol-of-harvest.png"
+    img: `${imgDir}/symbol-of-harvest.png`
   },
   {
     label: "Symbol of ancestors",
     path: "symbol-of-ancestors",
     title: "ancestors",
-    img: "./assets/img/symbol-of-ancestors.png"
+    img: `${imgDir}/symbol-of-ancestors.png`
   }
 ];
 
@@ -30,6 +35,6 @@ export const COMPOSITIONS = [
     label: "Symbol of love",
     path: "symbol-of-love",
     title: "symbol of love",
-    img: "./assets/img/symbol-of-love.png"
+    img: `${imgDir}/symbol-of-love.png`
   }
 ];
