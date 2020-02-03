15149a505499e09efccbec21fb96926de5a89615
fetchenko <30293847+fedchenko273@users.noreply.github.com>
2020-02-03 23:03:39 +0300

    add women symbol

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,34 +1,28 @@
-const imgDir = "/assets/images/"
+const imgDir = "/assets/images/";
 
 export const SYMBOL_OPTIONS = [
   {
-    label: "Spring and youth",
     path: "symbol-of-spring",
-    title: "spring and youth",
     img: `${imgDir}/symbol-of-spring.webp`
   },
   {
-    label: "Wealth",
     path: "symbol-of-wealth",
-    title: "wealth",
     img: `${imgDir}/symbol-of-wealth.webp`
   },
   {
-    label: "The sun and the earth",
     path: "symbol-of-harvest",
-    title: "harvest",
     img: `${imgDir}/symbol-of-harvest.webp`
   },
   {
-    label: "Ancestors",
     path: "symbol-of-ancestors",
-    title: "ancestors",
     img: `${imgDir}/symbol-of-ancestors.webp`
   },
   {
-    label: "Love",
     path: "symbol-of-love",
-    title: "symbol of love",
+    img: `${imgDir}/symbol-of-love.webp`
+  },
+  {
+    path: "symbol-of-women",
     img: `${imgDir}/symbol-of-love.webp`
   }
 ];
